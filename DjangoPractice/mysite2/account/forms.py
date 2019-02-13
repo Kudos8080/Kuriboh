67eb427e96a6e41f86266f358a23a31610048975
MicahXIE <bin.xie.sg@gmail.com>
2019-02-13 23:19:18 +0800

    commit

--- DjangoPractice/mysite2/account/forms.py
+++ DjangoPractice/mysite2/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 class LoginForm(forms.Form):
 	username = forms.CharField()
@@ -22,4 +23,9 @@ class RegistrationForm(forms.ModelForm):
 			raise forms.ValidationError("passwords do not match.")
 
 		return cd['password2']
+
+class UserProfileForm(forms.ModelForm):
+	class Meta:
+		model = UserProfile
+		fields = ("phone", "birth")
 
